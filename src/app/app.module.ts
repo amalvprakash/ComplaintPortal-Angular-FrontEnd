00e9667161a1b05160e44ababb09d4cf7ce85ec4
Amal <amalprakash2025@gmail.com>
2022-12-09 15:32:26 +0530

    routing completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,32 @@ import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserRegComponent } from './user-reg/user-reg.component';
 import { UserHomeComponent } from './user-home/user-home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+const myRoute:Routes = [
+  {
+    path:"",
+    component:AdminLoginComponent
+  },
+  {
+    path:"adminHome",
+    component:AdminHomeComponent
+  },
+  {
+    path:"userLogin",
+    component:UserLoginComponent
+  },
+  {
+    path:"userReg",
+    component:UserRegComponent
+  },
+  {
+    path:"userHome",
+    component:UserHomeComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +45,10 @@ import { UserHomeComponent } from './user-home/user-home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
